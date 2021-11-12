53ad660be7d0c318172cbf5f5e3044f017e14d3a
Renan Pallin <renanpallin@gmail.com>
2021-11-12 15:13:03 -0300

    Ready to deploy

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+
+import { Button } from '@material-ui/core';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+        Inscreva-se no canal, meu parça!
+        <Button
+          style={{
+            marginTop: '20px',
+            backgroundColor: '#69e21d',
+            color: 'black',
+          }}
+          variant="contained"
+          color="success"
         >
-          Learn React
-        </a>
+          Inscreva-se!
+        </Button>
       </header>
+      <footer>@renanpallin.dev</footer>
     </div>
   );
 }
