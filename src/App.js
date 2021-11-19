97b81432d81cabda59b9bef8ff2aabbdd8e74572
Renan Pallin <renanpallin@gmail.com>
2021-11-19 17:38:53 -0300

    Consumindo da API

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
 import './App.css';
 
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 import { Button } from '@material-ui/core';
 
+const api = process.env.REACT_APP_API_URL;
+
 function App() {
+  const [message, setMessage] = useState('');
+
+  useEffect(() => {
+    async function getMessage() {
+      const { data } = await axios.get(api);
+      setMessage(data.message);
+    }
+    getMessage();
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-        Inscreva-se no canal, meu parça!
+        {message}
         <Button
           style={{
             marginTop: '20px',
